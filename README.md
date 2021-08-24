fbc55d6be5af38fdac773dbb0aceeb59b086aa87
Jing <jing.c.geo@gmail.com>
2021-08-24 17:14:36 -0400

    refactor example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 5914-week-1
 
 pip install --upgrade "ibm-watson>=5.2.0"
+
+
+
+python source.py
--- source.py
+++ source.py
@@ -2,17 +2,27 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import json
 
-api_key = ''
 
-authenticator = IAMAuthenticator(api_key)
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
+def setup_translator():
+    api_key = ''
 
-language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+    authenticator = IAMAuthenticator(api_key)
+    language_translator = LanguageTranslatorV3(
+        version='2018-05-01',
+        authenticator=authenticator
+    )
 
-translation = language_translator.translate(
-    text='Hello, how are you today?',
-    model_id='en-es').get_result()
-print(json.dumps(translation, indent=2, ensure_ascii=False))
+    language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+    return language_translator
+
+    
+def run_example(language_translator):
+    translation = language_translator.translate(
+        text='Hello, how are you today?',
+        model_id='en-es').get_result()
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+if __name__ == "__main__":
+    translator = setup_translator()
+    run_example(translator)
+
