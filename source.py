9895b249670af6a2043921449fa3c4e56214dd12
Atharva <doshi.84@osu.edu>
2021-08-26 16:34:33 -0400

    intial version

--- source.py
+++ source.py
@@ -5,7 +5,7 @@ import random
 
 
 def setup_translator():
-    api_key = ''
+    api_key = 'p_8R7_hWnw_MOwrbqNoKgpoiBfQFy-O9xWXxBpC9bLDA'
 
     authenticator = IAMAuthenticator(api_key)
     language_translator = LanguageTranslatorV3(
@@ -26,7 +26,7 @@ def run_example(language_translator):
     print(translation['translations'][0]['translation'])
 
 def read_input():
-    print("Enter your vocab words as a space-separated list:")
+    print("Enter your vocab words as a space-separated list: ")
     user_input = input()
     words = user_input.split(" ")
     return words
@@ -35,16 +35,17 @@ def read_input():
 
 
 def do_quiz(language_translator, words):
-    def do_question():
-        # pick word and translate
+    # pick word and 
+    while True:
+        words_copy = words[:]
         picked_word = random.choice(words_copy)
-        response = language_translator.translate(text=picked_word, model_id='en_us').get_result()
+        original_word = picked_word
+        response = language_translator.translate(text=picked_word, model_id='en-es').get_result()
         translated_word = response['translations'][0]['translation']
 
         # get possible answers
         choices = []
         choices.append(picked_word)
-        words_copy = words[:]
         words_copy.remove(picked_word)
 
         for _ in range(3):
@@ -54,10 +55,27 @@ def do_quiz(language_translator, words):
             words_copy.remove(picked_word)
 
         random.shuffle(choices)
+        print(choices)
+
+        print("One word was randomly chosen from your vocab words and translated to Spanish." +
+        "The translated word is: "  + translated_word + ". What is this word in English?")
+        for i in range(4):
+            print(str(i+1) + ") " + choices[i])
+        user_choice = int(input())
+        while(choices[user_choice-1] != (original_word)):
+            print("Incorrect, please try again")
+            user_choice = int(input())
+        print("Correct")
+        quit = input("Would you like to quit (y/n)?")
+        if(quit == 'y'): break
+        
+    
+    
+    
 
 
 
-    if len(words < 4):
+    if len(words) < 4:
         print("Not enough words - minimum of 4 - exiting")
         return
     
@@ -66,7 +84,7 @@ def do_quiz(language_translator, words):
         
 if __name__ == "__main__":
     translator = setup_translator()
-    run_example(translator)
-    #words = read_input()
-    #do_quiz(translator, words)
+    #run_example(translator)
+    words = read_input()
+    do_quiz(translator, words)
 
